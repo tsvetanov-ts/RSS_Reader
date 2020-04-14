808da89cd5e988d3910e05af14f37d0b834f2cfc
Tsvetan Tsvetanov <tsvetanov.19@gmail.com>
2020-04-14 17:37:17 +0300

    add routes and gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :feeds
+  root 'feeds#index'
+  resources :feeds do
+    member do
+      resources :entries, only: [:index, :show]
+    end
+  end
+
+  resources :news
+
 end
