057ff7c1bf2fe5607dd5738bcb484d779c5c02a1
Tsvetan Tsvetanov <tsvetanov.19@gmail.com>
2020-04-16 21:22:44 +0300

    spec wip

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Feed, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes"
+  it "is not valid without a title"
+  it "is not valid without url"
+
 end
